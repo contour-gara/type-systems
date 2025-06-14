21689963f1ed2296a1385b31b08968bd6b4bc896
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-06-14 18:55:28 +0900

    テスト追加

--- 2/arith.ts
+++ 2/arith.ts
@@ -1,4 +1,5 @@
 import { parseArith } from "npm:tiny-ts-parser";
+import { assertEquals, assertThrows } from "jsr:@std/assert";
 
 type Type = { tag: "Boolean" } | { tag: "Number" };
 
@@ -17,11 +18,11 @@ function typecheck(t: Term): Type {
             return { tag: "Boolean" };
         case "if": {
             const condTy = typecheck(t.cond);
-            if (condTy.tag !== "Boolean") throw "boolean expected";
+            if (condTy.tag !== "Boolean") throw new Error("boolean expected");
             const thnTy = typecheck(t.thn);
             const elsTy = typecheck(t.els);
             if (thnTy.tag !== elsTy.tag) {
-                throw "then and else have different types";
+                throw new Error("then and else have different types");
             }
             return thnTy;
         }
@@ -29,12 +30,59 @@ function typecheck(t: Term): Type {
             return { tag: "Number" };
         case "add": {
             const leftTy = typecheck(t.left);
-            if (leftTy.tag !== "Number") throw "number expected";
+            if (leftTy.tag !== "Number") throw new Error("number expected");
             const rightTy = typecheck(t.right);
-            if (rightTy.tag !== "Number") throw "number expected";
+            if (rightTy.tag !== "Number") throw new Error("number expected");
             return { tag: "Number" };
         }
     }
 }
 
-console.log(typecheck(parseArith("true ? true : false")));
+console.log(typecheck(parseArith("true ? 1 : 2")));
+
+Deno.test("Number 同士の足し算の場合、Number が返る", () => {
+    const actual = typecheck(parseArith("1 + 2"));
+    assertEquals(actual, { tag: "Number" });
+});
+
+Deno.test("条件式で返す型が Boolean の場合、Boolean が返る", () => {
+    const actual = typecheck(parseArith("true ? true : false"));
+    assertEquals(actual, { tag: "Boolean" });
+});
+
+Deno.test("条件式で返す型が Number の場合、Number が返る", () => {
+    const actual = typecheck(parseArith("true ? 1 : 1 + 2"));
+    assertEquals(actual, { tag: "Number" });
+});
+
+Deno.test("足し算の左項が Boolean の場合、例外が返る", () => {
+    assertThrows(
+        () => typecheck(parseArith("true + 2")),
+        Error,
+        "number expected"
+    );
+});
+
+Deno.test("足し算の右項が Boolean の場合、例外が返る", () => {
+    assertThrows(
+        () => typecheck(parseArith("2 + false")),
+        Error,
+        "number expected"
+    );
+});
+
+Deno.test("条件式の条件が Number の場合、例外が返る", () => {
+    assertThrows(
+        () => typecheck(parseArith("1 + 2 ? 1 : 2")),
+        Error,
+        "boolean expected"
+    );
+});
+
+Deno.test("条件式の返り値の型が異なる場合、例外が返る", () => {
+    assertThrows(
+        () => typecheck(parseArith("true ? 1 : false")),
+        Error,
+        "then and else have different types"
+    );
+});
--- 2/exercise.ts
+++ 2/exercise.ts
@@ -1,4 +1,5 @@
 import { parseArith } from "npm:tiny-ts-parser";
+import { assertEquals, assertThrows } from "jsr:@std/assert";
 
 type Type = { tag: "Boolean" } | { tag: "Number" };
 
@@ -20,7 +21,7 @@ function typecheck(t: Term): Type {
             const thnTy = typecheck(t.thn);
             const elsTy = typecheck(t.els);
             if (thnTy.tag !== elsTy.tag) {
-                throw "then and else have different types";
+                throw new Error("then and else have different types");
             }
             return thnTy;
         }
@@ -28,12 +29,67 @@ function typecheck(t: Term): Type {
             return { tag: "Number" };
         case "add": {
             const leftTy = typecheck(t.left);
-            if (leftTy.tag !== "Number") throw "number expected";
+            if (leftTy.tag !== "Number") throw new Error("number expected");
             const rightTy = typecheck(t.right);
-            if (rightTy.tag !== "Number") throw "number expected";
+            if (rightTy.tag !== "Number") throw new Error("number expected");
             return { tag: "Number" };
         }
     }
 }
 
-console.log(typecheck(parseArith("1 + true ? true : false")));
+console.log(typecheck(parseArith("1 + 5 ? true : false")));
+
+Deno.test("Number 同士の足し算の場合、Number が返る", () => {
+    const actual = typecheck(parseArith("1 + 2"));
+    assertEquals(actual, { tag: "Number" });
+});
+
+Deno.test("条件式で返す型が Boolean の場合、Boolean が返る", () => {
+    const actual = typecheck(parseArith("true ? true : false"));
+    assertEquals(actual, { tag: "Boolean" });
+});
+
+Deno.test("条件式で返す型が Number の場合、Number が返る", () => {
+    const actual = typecheck(parseArith("true ? 1 : 1 + 2"));
+    assertEquals(actual, { tag: "Number" });
+});
+
+Deno.test("足し算の左項が Boolean の場合、例外が返る", () => {
+    assertThrows(
+        () => typecheck(parseArith("true + 2")),
+        Error,
+        "number expected"
+    );
+});
+
+Deno.test("足し算の右項が Boolean の場合、例外が返る", () => {
+    assertThrows(
+        () => typecheck(parseArith("2 + false")),
+        Error,
+        "number expected"
+    );
+});
+
+Deno.test("条件式の返り値の型が異なる場合、例外が返る", () => {
+    assertThrows(
+        () => typecheck(parseArith("true ? 1 : false")),
+        Error,
+        "then and else have different types"
+    );
+});
+
+Deno.test(
+    "条件式の条件が Number で返す型が Number の場合、Number が返る",
+    () => {
+        const actual = typecheck(parseArith("1 + 2 ? 1 : 2"));
+        assertEquals(actual, { tag: "Number" });
+    }
+);
+
+Deno.test("条件式の条件が Boolean と Number の足し算の場合、例外が返る", () => {
+    assertThrows(
+        () => typecheck(parseArith("true + 1 ? 1 : 2")),
+        Error,
+        "number expected"
+    );
+});
